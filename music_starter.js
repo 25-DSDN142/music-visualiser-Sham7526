1317baa150a45759b5609734f1e2ed01043de076
Sham7526 <samuelhadfield06@gmail.com>
2025-08-05 12:26:24 +1200

    Added Drum and Bass

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,111 @@
+let drum_y = 750
+let drum_x = 500
+let drum_size
+let drum_s = 75
+
+let bass_y = 0
+let bass_size
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
+  background(109, 29, 222)
+   textFont('Verdana'); // please use CSS safe fonts
+   rectMode(TOP);
+   textSize(24);
+
+ // display "words"
    textAlign(CENTER);
    textSize(vocal);
-   text(words, width/2, height/3);
+   text(words, width/2, height/2);
+ 
+ // color
+   let blue = color(5, 162, 252);
+   let light_blue = color(180, 220, 240);
+   let purple = color(109, 29, 222);
+ 
+ // display "drums"
+function Drum(){
+drum_size = drum + drum_s
+   stroke(light_blue);
+   strokeWeight(2);
+   fill(blue);
+   ellipse(drum_x, drum_y, drum_size + 175);
+   noStroke();
+   fill(light_blue);
+   ellipse(drum_x, drum_y, drum_s + 175);
+   stroke(blue);
+   fill(purple);
+   ellipse(drum_x, drum_y, drum_size);
 }
+ // display "bass" 
+function Bass(){
+bass_size = bass + 200
+x = 100;
+   let bass_x = map(x, 100, 200, 0, 200);
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y, bass_x, 30, bass_size);
+   
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 32, bass_x, 30, bass_size - 50);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 64, bass_x, 30, bass_size - 70);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 96, bass_x, 30, bass_size + 20);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 128, bass_x, 30, bass_size);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 160, bass_x, 30, bass_size - 20);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 192, bass_x, 30, bass_size + 10);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 224, bass_x, 30, bass_size + 5);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 256, bass_x, 30, bass_size);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 288, bass_x, 30, bass_size - 20);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 320, bass_x, 30, bass_size - 5);
+
+   stroke(blue);
+   fill(light_blue);
+   strokeWeight(2);
+   rect(bass_y + 256, bass_x, 30, bass_size);
+
+}
+
+Drum()
+Bass()
+
+}
+
